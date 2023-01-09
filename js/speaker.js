043e32fc6898eb4e05025a84e5145f5853cfc0c0
Sun Seng David TAN <sutan@redhat.com>
2023-01-09 13:26:40 +0100

    feat: adding more yb speakers
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

--- js/speaker.js
+++ js/speaker.js
@@ -6,6 +6,24 @@ const speakers = [
         title: "19h30 : 204VS404 le duel du bon code http"
     },
     {
+        name: "Hicham Bellahcene",
+        avatar: "https://pbs.twimg.com/profile_images/1490013462881357827/Vv_J66Kg_400x400.png",
+        contact: "@hbellahc",
+        title: "19h50 : Using lambda with Java Streams"
+    },
+    {
+        name: "Coline Therial",
+        avatar: "/img/yb-coline.jpg",
+        contact: "",
+        title: "20h10 : Faire du web ou du mobile ? Et pourquoi pas les deux !"
+    },
+    {
+        name: "Martin Pernollet",
+        avatar: "/img/yb-martin.jpg",
+        contact: "",
+        title: "21h00 : Avenir du rendu 3D sur la JVM avec Panama (JEP-412)"
+    },
+    {
         name: "Pierre Cheucle",
         avatar: "/img/yb-pierre.gif",
         contact: "",
