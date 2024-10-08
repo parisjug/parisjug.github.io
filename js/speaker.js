a746b96f67e90c482cf6202c159ac4c6261cb4de
Charles Sabourdin <1420213+kanedafromparis@users.noreply.github.com>
2024-10-08 17:04:19 +0200

    Update speaker.js - october 2024

--- js/speaker.js
+++ js/speaker.js
@@ -1,15 +1,21 @@
 const speakers = [
     {
-        name: "Sun Tan",
-        title: "Super developer at Sciam",
-        contact: "@__sunix_",
-        avatar: "https://pbs.twimg.com/profile_images/1640301246123761664/aVIYwE4b_400x400.jpg"
+        name: "Alban Clevy",
+        title: "développeur, chef de projet, responsable d’équipe, architecte, techlead",
+        contact: "",
+        avatar: "https://pbs.twimg.com/profile_images/1268663631413215235/VSq2QFAV_400x400.jpg"
     },
     {
-        name: "Charles Sabourdin",
-        title: "Super great developer at Sciam",
-        contact: "@kanedafromparis",
-        avatar: "https://pbs.twimg.com/profile_images/852073008772648960/OHbbEu8Y_400x400.jpg"
+        name: "Hervé Letourneur",
+        title: "Practice Leader Java",
+        contact: "",
+        avatar: "https://pbs.twimg.com/profile_images/1268663631413215235/VSq2QFAV_400x400.jpg"
+    }, 
+    {
+        name: "Christophe Agoero",
+        title: "Architecte Java",
+        contact: "",
+        avatar: "https://www.parisjug.org/about/introduction/christophe-agoero.png"
     }, 
 ]
 
