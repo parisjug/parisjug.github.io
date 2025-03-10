17357f6cf75319ec2905ca747970409f75fc8958
Sun S. D. Tan <sunix@sunix.org>
2025-03-10 21:43:26 +0100

    Update speaker.js march 2025

--- js/speaker.js
+++ js/speaker.js
@@ -1,15 +1,15 @@
 const speakers = [
     {
-        name: "Jean-Philippe Bempel",
-        title: "Senior Software Engineer chez Datadog",
-        contact: "@jpbempel.bsky.social",
-        avatar: "https://pbs.twimg.com/profile_images/1019190216995811328/YARM51Fl_400x400.jpg"
+        name: "Jean-Christophe Sirot",
+        title: "Staff engineer chez Decathlon",
+        contact: "@sirot.org",
+        avatar: "https://cdn.bsky.app/img/avatar/plain/did:plc:3n634emyl24rng73wrsrcqef/bafkreic7qle5gqsrjch6zb25x7nx3mmcbr2j3j4jf25o4l3zjcm2yul7qu@jpeg"
     },
     {
-        name: "William Montaz",
-        title: "SRE chez Criteo",
-        contact: "@willymontaz",
-        avatar: "https://pbs.twimg.com/profile_images/535013706048864256/rkvDpOhg_400x400.jpeg"
+        name: "Bruce Bujon",
+        title: "Senior Software Engineer chez Datadog",
+        contact: "@hardcoding.fr",
+        avatar: "https://cdn.bsky.app/img/avatar/plain/did:plc:unv2ly5becx5ykia5urtgdz4/bafkreigg3zv3ifian4hugilmdldoblnt5i37qqd226pmur3aqeyrobasl4@jpeg"
     }
 ]
 
